0b5bf82de5cffb39bc1b4e816a3a85e2e6fb6fe8
Jaypal Solanki <jaypsl263@gmail.com>
2018-07-01 18:44:19 -0400

    made changes in js file

--- index.js
+++ index.js
@@ -16,11 +16,37 @@ app.get('/', function (req,res) {
 })
 
 app.get('/webhook/', function (req,res) {
-  if(req.query['hub.verify_token']=== "jayso"){
-    res.send(req.query['hub.challenge'])
+  let messaging_events = req.body.entry[0].messaging;
+for (let i=0; i<messaging_events.length;i++){
+  let event = messaging_events[i]
+  let sender = event.sender.id;
+  if(event.message && event.message.text){
+    let text = event.message.text;
+    sendText(sender, "text echo" + text.substring(0,100))
   }
-  res.send('wrongtoken')
+}
+res.sendStatus(200);
+
 })
+
+function sendText(sender,text){
+  let messageData = {text : text}
+  request({
+    url : "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token :  token}
+    method: "POST"
+    json: {
+      recipient : {id: sender},
+      message: messageData
+    }
+  }, function(error,response, body){
+    if(error){
+      console.log('sending error');
+    }else if(response.body.error){
+      console.log("response body error");
+    }
+  })
+}
 
 app.listen(app.get('port'),function () {
   console.log('running at port' + app.get( 'port' ));
